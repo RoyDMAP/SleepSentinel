d71a14e1c3bd97e8d7a365db7619a57b6a9016e6
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-08 21:34:45 -0700

    Initial Commit

---
+++ SleepSentinel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SleepSentinel
+//
+//  Created by Roy Dimapilis on 10/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SleepSentinel/SleepSentinelApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepSentinelApp.swift
+//  SleepSentinel
+//
+//  Created by Roy Dimapilis on 10/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SleepSentinelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
